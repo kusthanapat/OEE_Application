356fcde17f61ab8655662b2e15aeaa7a85cb4d66
Kusthanapat <forkus156155@gmail.com>
2025-09-05 17:13:53 +0700

    third commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: [
     "./templates/**/*.html", // สำคัญ!
-<<<<<<< HEAD
     "./static/**/*.{js,css}", // ถ้ามี JS ที่ใช้ Tailwind classes
-=======
-    "./static/js/**/*.js", // ถ้ามี JS ที่ใช้ Tailwind classes
->>>>>>> 1efc0888d317b75d222fc46ccbbda82369f5f5c4
   ],
   theme: {
     extend: {},
